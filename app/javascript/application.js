fac75a6a81b427d123d3f266a61660f7c717f447
Ayslan Marcelino <43254034+ayslanmarcelino@users.noreply.github.com>
2023-04-23 14:30:19 -0300

    Initial commit

---
+++ app/javascript/application.js
@@ -0,0 +1,12 @@
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
+import "./controllers"
+import Swal from 'sweetalert2';
+import '../../lib/assets/javascripts/confirm';
+import '../../lib/assets/javascripts/masks';
+import '../../lib/assets/javascripts/fill_address';
+import '../../lib/assets/javascripts/required_fields';
+import '../../lib/assets/javascripts/validate_document_number/person';
+import 'chartkick/chart.js'
+
+window.Swal = Swal;
