a371a9d4bffcdd3670ad3d1b855dd90f295e457b
Ayslan Marcelino <ayslan_marcelino@outlook.com>
2023-04-24 17:00:57 -0300

    Add controller

--- app/controllers/supplies_controller.rb
+++ app/controllers/supplies_controller.rb
@@ -16,9 +16,28 @@ class SuppliesController < ApplicationController
 
   def create; end
 
+  def new
+    @supply = Supply.new
+  end
+
+  def create
+    @supply = Supply.new(supply_params)
+
+    if @supply.save
+      redirect_success(path: supplies_path, action: 'criado')
+    else
+      render(:new, status: :unprocessable_entity)
+    end
+  end
+
   private
 
   def supply_params
     params.require(:supply).permit(Supply.permitted_params).merge(created_by: current_user)
+  end
+
+  def redirect_success(path:, action:)
+    redirect_to(path)
+    flash[:success] = "Suprimento #{action} com sucesso."
   end
 end
