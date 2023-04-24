a03a1bb49a272d378b9f87463c0d0f5d40ce7765
Ayslan Marcelino <ayslan_marcelino@outlook.com>
2023-04-24 11:23:37 -0300

    Add to view

--- app/models/supply.rb
+++ app/models/supply.rb
@@ -2,10 +2,20 @@
 #
 # Table name: supplies
 #
-#  id         :bigint           not null, primary key
-#  name       :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id            :bigint           not null, primary key
+#  name          :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  created_by_id :bigint
+#
+# Indexes
+#
+#  index_supplies_on_created_by_id  (created_by_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (created_by_id => users.id)
 #
 class Supply < ApplicationRecord
+  belongs_to :created_by, class_name: 'User', optional: true
 end
