5a6200a3696ca1b2755e8151f9b05df68bda540a
Ayslan Marcelino <43254034+ayslanmarcelino@users.noreply.github.com>
2023-04-25 17:41:31 -0300

    SS-17 - Lote - Create (#9)
    * Add routes
    
    * Add controller and remove useless param model
    
    * Add button to redirect
    
    * Add form
    
    * Fix translate
    
    * Add ability
    
    * Fix specs

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -1,4 +1,6 @@
 class BatchesController < ApplicationController
+  before_action :supplies, only: [:new, :create]
+
   def index
     @query = Batch.includes(:supply, created_by: :person)
                   .order(created_at: :desc)
@@ -7,5 +9,34 @@ class BatchesController < ApplicationController
                   .ransack(params[:q])
 
     @batches = @query.result(distinct: false)
+  end
+
+  def new
+    @batch = Batch.new
+  end
+
+  def create
+    @batch = Batch.new(batch_params)
+
+    if @batch.save
+      redirect_success(path: batches_path, action: 'criado')
+    else
+      render(:new, status: :unprocessable_entity)
+    end
+  end
+
+  private
+
+  def batch_params
+    params.require(:batch).permit(Batch.permitted_params).merge(created_by: current_user, remaining: params[:batch][:amount])
+  end
+
+  def redirect_success(path:, action:)
+    redirect_to(path)
+    flash[:success] = "Lote #{action} com sucesso."
+  end
+
+  def supplies
+    @supplies ||= Supply.all
   end
 end
