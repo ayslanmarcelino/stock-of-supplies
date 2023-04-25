9456b1d0a433a51a8ef70890bc30046a30416fe3
Ayslan Marcelino <ayslan_marcelino@outlook.com>
2023-04-24 23:48:12 -0300

    Fix sort ransack and add validates arrived_date;

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -33,6 +33,7 @@ class Batch < ApplicationRecord
             presence: true
   validates :identifier, uniqueness: true
   validates :amount, numericality: { greater_than: 0 }
+  validates :arrived_date, comparison: { less_than_or_equal_to: Date.current }
   validates :expiration_date, comparison: { greater_than_or_equal_to: Date.current }
 
   def self.permitted_params
