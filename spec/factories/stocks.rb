df7b70c73725f9f65099238e41002df1e3d6b46e
Ayslan Marcelino <ayslan_marcelino@outlook.com>
2023-04-25 23:41:40 -0300

    Fix factory

--- spec/factories/stocks.rb
+++ spec/factories/stocks.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :stock do
-    
+    amount { rand(0..10) }
+    kind_cd { [:input, :output].sample }
+    supply { create(:supply) }
+    unit { create(:unit) }
   end
 end
