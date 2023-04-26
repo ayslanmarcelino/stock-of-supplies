32d6458221b5f19fd2a18e785f217f39b9882667
Ayslan Marcelino <ayslan_marcelino@outlook.com>
2023-04-26 11:47:52 -0300

    Fix n+1 query

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -4,7 +4,7 @@ class StocksController < ApplicationController
   load_and_authorize_resource
 
   def index
-    @query = Stock.includes(:supply)
+    @query = Stock.includes(:supply, :created_by)
                   .order(created_at: :desc)
                   .accessible_by(current_ability)
                   .page(params[:page])
