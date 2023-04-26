6bebb3fffcff5bc0937582d87aca6db0b210665b
Ayslan Marcelino <ayslan_marcelino@outlook.com>
2023-04-25 23:42:30 -0300

    Fix model

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,6 +4,7 @@
 #
 #  id         :bigint           not null, primary key
 #  amount     :integer
+#  kind_cd    :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  supply_id  :bigint
@@ -20,6 +21,17 @@
 #  fk_rails_...  (unit_id => units.id)
 #
 class Stock < ApplicationRecord
+  KINDS = [
+    [I18n.t('activerecord.attributes.stock.kind_list.input'), :input],
+    [I18n.t('activerecord.attributes.stock.kind_list.output'), :output]
+  ].freeze
+
   belongs_to :supply
   belongs_to :unit
+
+  as_enum :kind, [:input, :output], prefix: true, map: :string
+
+  def translated_kind
+    translated = I18n.t("activerecord.attributes.stock.kind_list.#{kind}")
+  end
 end
