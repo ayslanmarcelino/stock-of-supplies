7ec5a23dae535fbcb7deb728232582831d1f942a
Ayslan Marcelino <ayslan_marcelino@outlook.com>
2023-04-26 13:11:08 -0300

    Add frozen_string_literal

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class DashboardController < ApplicationController
   def index
     @chart = Stocks::Chart::Build.call(user: current_user)
--- app/controllers/user/roles_controller.rb
+++ app/controllers/user/roles_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class User::RolesController < ApplicationController
   load_and_authorize_resource
 
