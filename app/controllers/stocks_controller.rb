5e80f8c68ca86e48bd9508784a5a5cb5bc9f53ed
Ayslan Marcelino <ayslan_marcelino@outlook.com>
2023-04-26 00:35:44 -0300

    Add controller

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,3 +1,16 @@
+# frozen_string_literal: true
+
 class StocksController < ApplicationController
-  def index; end
+  load_and_authorize_resource
+
+  def index
+    @query = Stock.includes(:supply)
+                  .order(created_at: :desc)
+                  .accessible_by(current_ability)
+                  .page(params[:page])
+                  .ransack(params[:q])
+
+    @stocks = @query.result(distinct: false)
+    @chart = Stocks::Chart::Create.call(user: current_user)
+  end
 end
