697ced35dcfa051571c4d36367a03391c425cc87
Ayslan Marcelino <ayslan_marcelino@outlook.com>
2023-04-26 00:35:58 -0300

    Add service to build data

---
+++ app/services/stocks/chart/create.rb
@@ -0,0 +1,52 @@
+module Stocks
+  module Chart
+    class Create < ApplicationService
+      def initialize(user:)
+        @user = user
+        @labels = []
+        @values = []
+      end
+
+      def call
+        data
+      end
+
+      private
+
+      def data
+        feed_arrays
+
+        {
+          labels: @labels,
+          values: @values
+        }
+      end
+
+      def feed_arrays
+        supplies_by_stock.each do |supply|
+          input_amount = input_values[supply.id]&.sum(&:amount) || 0
+          output_amount = output_values[supply.id]&.sum(&:amount) || 0
+
+          @labels << supply.name
+          @values << input_amount - output_amount
+        end
+      end
+
+      def supplies_by_stock
+        @supplies_by_stock ||= Supply.where(id: unit_stock.pluck(:supply_id).uniq).order(:name)
+      end
+
+      def output_values
+        @output_values ||= unit_stock.kind_outputs.group_by(&:supply_id)
+      end
+
+      def input_values
+        @input_values ||= unit_stock.kind_inputs.group_by(&:supply_id)
+      end
+
+      def unit_stock
+        @unit_stock ||= Stock.where(unit: @user.current_unit)
+      end
+    end
+  end
+end
