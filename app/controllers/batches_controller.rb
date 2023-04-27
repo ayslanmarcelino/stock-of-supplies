3c3b5ef5d0f61f2d26695435a1f23ef7ee7694db
Ayslan Marcelino <ayslan_marcelino@outlook.com>
2023-04-27 17:37:44 -0300

    Add controller

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -23,7 +23,12 @@ class BatchesController < ApplicationController
     @batch = Batch.new(batch_params)
 
     if @batch.save
-      create_input_stock!(amount: @batch.amount, arrived_date: @batch.arrived_date)
+      create_stock!(
+        amount: @batch.amount,
+        arrived_date: @batch.arrived_date,
+        kind: :input,
+        reason: 'Recebido pelo Estado'
+      )
       redirect_success(path: batches_path, action: 'criado')
     else
       render(:new, status: :unprocessable_entity)
@@ -31,14 +36,47 @@ class BatchesController < ApplicationController
   end
 
   def increment_amount
-    assign_data
+    assign_increment_data
 
     if resource.valid?
       resource.save!
-      create_input_stock!(amount: params[:batch][:amount], arrived_date: params[:batch][:arrived_date])
+      create_stock!(
+        amount: params[:batch][:amount],
+        arrived_date: params[:batch][:arrived_date],
+        kind: :input,
+        reason: 'Recebido pelo Estado'
+      )
       flash[:success] = "Valor adicionado ao lote #{resource.identifier}."
     else
       flash[:alert] = batch.errors.full_messages.to_sentence
+    end
+
+    redirect_to(batches_path)
+  end
+
+  def new_output
+    if params[:batch][:remaining].to_i <= 0
+      flash[:alert] = 'Quantidade deve ser maior que 0'
+      redirect_to(batches_path)
+
+      return
+    end
+
+    assign_new_output_data
+
+    if @batch.valid? && @batch.remaining >= 0
+      @batch.save!
+      create_stock!(
+        amount: params[:batch][:remaining],
+        arrived_date: params[:batch][:arrived_date],
+        kind: :output,
+        reason: 'Utilizado em pacientes'
+      )
+      flash[:success] = "Saída do lote #{resource.identifier} criada com sucesso."
+    elsif @batch.remaining <= 0
+      flash[:alert] = 'Quantidade da saída ultrapassa a quantidade do estoque. Tente novamente com a quantidade correta.'
+    else
+      flash[:alert] = @batch.errors.full_messages.to_sentence
     end
 
     redirect_to(batches_path)
@@ -65,12 +103,12 @@ class BatchesController < ApplicationController
     @supplies ||= Supply.all
   end
 
-  def create_input_stock!(amount:, arrived_date:)
+  def create_stock!(amount:, arrived_date:, kind:, reason:)
     Stocks::Create.call(
       params: @batch,
       batch: @batch,
-      reason: 'Recebido pelo Estado',
-      kind: :input,
+      reason: reason,
+      kind: kind,
       amount: amount,
       arrived_date: arrived_date
     )
@@ -80,7 +118,9 @@ class BatchesController < ApplicationController
     @resource ||= Batch.find(params[:id])
   end
 
-  def assign_data
+  def assign_increment_data
+    return if params[:batch][:amount].to_i <= 0
+
     resource.amount += params[:batch][:amount].to_i
     resource.remaining += params[:batch][:amount].to_i
     resource.arrived_date = params[:batch][:arrived_date] if before?
@@ -88,5 +128,9 @@ class BatchesController < ApplicationController
 
   def before?
     resource.arrived_date < params[:batch][:arrived_date].to_date
+  end
+
+  def assign_new_output_data
+    @batch.remaining -= params[:batch][:remaining].to_i
   end
 end
