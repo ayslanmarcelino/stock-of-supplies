7d7a42965debfbbc482679970fab6ff7e08bfce2
Ayslan Marcelino <ayslan_marcelino@outlook.com>
2023-04-28 01:19:53 -0300

    Refactor controller and view

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -38,48 +38,29 @@ class BatchesController < ApplicationController
   def increment_amount
     assign_increment_data
 
-    if resource.valid?
-      resource.save!
-      create_stock!(
-        amount: params[:batch][:amount],
-        arrived_date: params[:batch][:arrived_date],
-        kind: :input,
-        reason: 'Recebido pelo Estado'
-      )
+    if input_created_successfully?
       flash[:success] = "Valor adicionado ao lote #{resource.identifier}."
+    elsif params[:batch][:amount].to_i <= 0
+      flash[:alert] = 'Quantidade deve ser maior que 0'
     else
-      flash[:alert] = batch.errors.full_messages.to_sentence
+      flash[:alert] = increment_amount_error_message
     end
 
-    redirect_to(batches_path)
+    redirect_to batches_path
   end
 
   def new_output
-    if params[:batch][:remaining].to_i <= 0
-      flash[:alert] = 'Quantidade deve ser maior que 0'
-      redirect_to(batches_path)
-
-      return
-    end
-
     assign_new_output_data
 
-    if @batch.valid? && @batch.remaining >= 0
-      @batch.save!
-      create_stock!(
-        amount: params[:batch][:remaining],
-        arrived_date: params[:batch][:arrived_date],
-        kind: :output,
-        reason: 'Utilizado em pacientes'
-      )
+    if params[:batch][:remaining].to_i <= 0
+      flash[:alert] = 'Quantidade deve ser maior que 0'
+    elsif output_created_successfully?
       flash[:success] = "Saída do lote #{resource.identifier} criada com sucesso."
-    elsif @batch.remaining <= 0
-      flash[:alert] = 'Quantidade da saída ultrapassa a quantidade do estoque. Tente novamente com a quantidade correta.'
     else
-      flash[:alert] = @batch.errors.full_messages.to_sentence
+      flash[:alert] = new_output_error_message
     end
 
-    redirect_to(batches_path)
+    redirect_to batches_path
   end
 
   private
@@ -132,5 +113,33 @@ class BatchesController < ApplicationController
 
   def assign_new_output_data
     @batch.remaining -= params[:batch][:remaining].to_i
+  end
+
+  def input_created_successfully?
+    resource.valid? &&
+      params[:batch][:amount].to_i.positive? &&
+      create_stock!(amount: params[:batch][:amount], arrived_date: params[:batch][:arrived_date], kind: :input,
+                    reason: 'Recebido pelo Estado') &&
+      resource.save!
+  end
+
+  def increment_amount_error_message
+    resource.errors.full_messages.to_sentence
+  end
+
+  def output_created_successfully?
+    @batch.valid? &&
+      @batch.remaining >= params[:batch][:remaining].to_i &&
+      create_stock!(amount: params[:batch][:remaining], arrived_date: params[:batch][:arrived_date], kind: :output,
+                    reason: 'Utilizado em pacientes') &&
+      @batch.save!
+  end
+
+  def new_output_error_message
+    if @batch.remaining <= 0
+      'Quantidade da saída ultrapassa a quantidade do estoque. Tente novamente com a quantidade correta.'
+    else
+      @batch.errors.full_messages.to_sentence
+    end
   end
 end
