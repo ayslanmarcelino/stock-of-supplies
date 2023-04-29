7a3bcf8454a9c15952155fffdb6f6d3100253c2b
Ayslan Marcelino <ayslan_marcelino@outlook.com>
2023-04-29 17:24:39 -0300

    Add model

---
+++ app/models/order.rb
@@ -0,0 +1,37 @@
+# == Schema Information
+#
+# Table name: orders
+#
+#  id              :bigint           not null, primary key
+#  amount          :string
+#  approval_date   :date
+#  delivery_date   :date
+#  reason          :string
+#  rejection_date  :date
+#  status          :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  approved_by_id  :bigint
+#  created_by_id   :bigint
+#  delivered_by_id :bigint
+#  rejected_by_id  :bigint
+#  stock_id        :bigint
+#
+# Indexes
+#
+#  index_orders_on_approved_by_id   (approved_by_id)
+#  index_orders_on_created_by_id    (created_by_id)
+#  index_orders_on_delivered_by_id  (delivered_by_id)
+#  index_orders_on_rejected_by_id   (rejected_by_id)
+#  index_orders_on_stock_id         (stock_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (approved_by_id => users.id)
+#  fk_rails_...  (created_by_id => users.id)
+#  fk_rails_...  (delivered_by_id => users.id)
+#  fk_rails_...  (rejected_by_id => users.id)
+#  fk_rails_...  (stock_id => stocks.id)
+#
+class Order < ApplicationRecord
+end
